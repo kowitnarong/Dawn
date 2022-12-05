af925abe7cf415c8ac31f1543c759ea05b7a7be1
Golffy183 <kowit_narongtuwapan@cmu.ac.th>
2022-12-05 20:17:12 +0700

    add Project file - add Test scene - add Movement and Camera system - add Input system

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kowit.GameDev4
+{
+    public class InputHandler : MonoBehaviour
+    {
+        public float horizontal;
+        public float vertical;
+        public float moveAmount;
+        public float mouseX;
+        public float mouseY;
+
+        public bool b_Input;
+
+        public bool rollFlag;
+        public bool isInteracting;
+
+
+        PlayerControls inputActions;
+        CameraHandler cameraHandler;
+
+
+        Vector2 movementInput;
+        Vector2 cameraInput;
+
+        public void Awake()
+        {
+            cameraHandler = CameraHandler.singleton;
+        }
+
+        private void FixedUpdate()
+        {
+            float delta = Time.fixedDeltaTime;
+
+            if (cameraHandler != null)
+            {
+                cameraHandler.FollowTarget(delta);
+                cameraHandler.HandleCameraRotation(delta, mouseX, mouseY);
+            }
+        }
+
+        public void OnEnable()
+        {
+            if (inputActions == null)
+            {
+                inputActions = new PlayerControls();
+                inputActions.PlayerMovement.Movement.performed += inputActions => movementInput = inputActions.ReadValue<Vector2>();
+                inputActions.PlayerMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+            }
+
+            inputActions.Enable();
+        }
+
+        public void OnDisable()
+        {
+            inputActions.Disable();
+        }
+
+        public void TickInput(float delta)
+        {
+            MoveInput(delta);
+            HandleRollInput(delta);
+        }
+
+        public void MoveInput(float delta)
+        {
+            // Get the input from the player
+            horizontal = movementInput.x;
+            vertical = movementInput.y;
+
+            moveAmount = Mathf.Clamp01(Mathf.Abs(horizontal) + Mathf.Abs(vertical));
+            mouseX = cameraInput.x;
+            mouseY = cameraInput.y;
+        }
+
+        private void HandleRollInput(float delta)
+        {
+            b_Input = inputActions.PlayerActions.Roll.phase == UnityEngine.InputSystem.InputActionPhase.Started;
+
+            if (b_Input)
+            {
+                rollFlag = true;
+            }
+        }
+    }
+}
