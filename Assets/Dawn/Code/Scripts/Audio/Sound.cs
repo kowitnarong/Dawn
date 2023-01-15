35c16f453a72f3463449b9ec08745c93d96b41bd
Golffy183 <kowit_narongtuwapan@cmu.ac.th>
2023-01-16 01:40:18 +0700

    Add sound system and scene manager (in progress)

---
+++ Assets/Dawn/Code/Scripts/Audio/Sound.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace GameDev4.Dawn
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+
+        public AudioClip clip;
+        public AudioMixerGroup mixerGroup;
+
+        [Range(0f, 1f)]
+        public float volume;
+        [Range(0.1f, 3f)]
+        public float pitch;
+        [Range(0f, 1f)]
+        public float spatial;
+
+        public bool loop;
+
+        [HideInInspector]
+        public AudioSource source;
+    }
+}
