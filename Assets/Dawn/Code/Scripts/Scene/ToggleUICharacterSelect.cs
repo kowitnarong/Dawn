2158ff78825eab47726bdcb3722d611d8e7a360b
Golffy183 <kowit_narongtuwapan@cmu.ac.th>
2023-01-18 19:31:10 +0700

    Update ConnectToServer scene -add new UI -add character UI select

---
+++ Assets/Dawn/Code/Scripts/Scene/ToggleUICharacterSelect.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace GameDev4.Dawn
+{
+    public class ToggleUICharacterSelect : MonoBehaviour
+    {
+        public List<Toggle> toggles;
+        public Button connectButton;
+        public TMP_InputField characterName;
+
+        [SerializeField] private int index = 0;
+        [SerializeField] protected bool isSelected = true;
+
+
+        public void Start()
+        {
+            toggles[0].isOn = true;
+            toggles[1].isOn = false;
+            toggles[2].isOn = false;
+        }
+
+        private void Update()
+        {
+            if (toggles[0].isOn == false && toggles[1].isOn == false && toggles[2].isOn == false)
+            {
+                isSelected = false;
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(characterName.text))
+                {
+                    isSelected = false;
+                }
+                else
+                {
+                    isSelected = true;
+                }
+            }
+            OnConnectEnable();
+        }
+
+        public void CharacterSummerSelected()
+        {
+            if (toggles[0].isOn == true)
+            {
+                toggles[0].isOn = true;
+                toggles[1].isOn = false;
+                toggles[2].isOn = false;
+                index = 0;
+            }
+        }
+
+        public void CharacterRainSelected()
+        {
+            if (toggles[1].isOn == true)
+            {
+                toggles[0].isOn = false;
+                toggles[1].isOn = true;
+                toggles[2].isOn = false;
+                index = 1;
+            }
+        }
+
+        public void CharacterWinterSelected()
+        {
+            if (toggles[2].isOn == true)
+            {
+                toggles[0].isOn = false;
+                toggles[1].isOn = false;
+                toggles[2].isOn = true;
+                index = 2;
+            }
+        }
+
+        private void OnConnectEnable()
+        {
+            if (isSelected == true)
+            {
+                connectButton.interactable = true;
+            }
+            else
+            {
+                connectButton.interactable = false;
+            }
+        }
+    }
+}
